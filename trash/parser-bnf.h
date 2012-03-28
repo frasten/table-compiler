edb167aee112d1ca25dd4b85fd0bda37a0237a25
Andrea Piccinelli <frasten@gmail.com>
2012-03-28 18:34:57 +0200

    Togliamo dalle scatole alcuni files del parser BNF.

---
+++ trash/parser-bnf.h
@@ -0,0 +1,48 @@
+int parse();
+void next();
+void parseerror();
+void match(int what);
+
+void parse_program();
+void parse_stat_list();
+void parse_stat();
+void parse_def_stat();
+void parse_id_list(char skip_first_id);
+void parse_type();
+void parse_atomic_type();
+void parse_table_type();
+void parse_attr_list();
+void parse_attr_decl();
+void parse_assign_stat();
+void parse_expr();
+void parse_bool_op();
+void parse_bool_term();
+void parse_comp_op();
+void parse_comp_term();
+void parse_low_bin_op();
+void parse_low_term();
+void parse_high_bin_op();
+void parse_factor();
+void parse_unary_op();
+
+void parse_join_op();
+void parse_project_op();
+void parse_select_op();
+void parse_exists_op();
+void parse_all_op();
+void parse_extend_op();
+void parse_update_op();
+void parse_rename_op();
+void parse_const();
+void parse_atomic_const();
+void parse_table_const();
+void parse_tuple_list();
+void parse_tuple_const();
+void parse_atomic_const_list();
+void parse_if_stat();
+void parse_else_part();
+void parse_while_stat();
+void parse_read_stat();
+void parse_specifier();
+void parse_write_stat();
+
